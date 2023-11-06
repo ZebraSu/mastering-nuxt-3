7e6ff52524bf58f0fbe770907ab64b2d129d3740
zihaosu <>
2023-11-06 15:26:03 +0800

    Initial commit

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,24 @@
+<template>
+  <iframe
+    width="560" 
+    height="315"
+    :src="`https://player.vimeo.com/video/${ props.videoId }`"
+    title="Video player"
+    allow="autoplay; accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+    frameborder="0"
+  >
+
+  </iframe>
+</template>
+
+<script lang="ts" setup>
+  const props = defineProps({
+    videoId: {
+      type: Number,
+      required: true,
+    },
+  })
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>hello Nuxt 3</div>
+</template>
